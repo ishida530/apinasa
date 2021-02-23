617dba45a7b86aa19b68c87ce89c5f297852e620
ishida530 <63319022+ishida530@users.noreply.github.com>
2021-02-23 17:44:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -35,65 +35,63 @@ document.getElementById("geocoder").appendChild(geocoder.onAdd(map));
 const placeholderInput = document.querySelector("input");
 placeholderInput.setAttribute("placeholder", "Enter the city...");
 
-input.addEventListener("keydown", async (e) => {
-  if (e.key === "Enter") {
-    inputCity = e.target.value;
-    let params = {
-      q: inputCity,
-      format: "json",
-    };
-    axios
-      .get("https://nominatim.openstreetmap.org/search", {
-        params: params,
-      })
-      .then(function (response) {
-        x = response.data[0].lat;
-        y = response.data[0].lon;
+input.addEventListener("change", async (e) => {
+  inputCity = e.target.value;
+  let params = {
+    q: inputCity,
+    format: "json",
+  };
+  axios
+    .get("https://nominatim.openstreetmap.org/search", {
+      params: params,
+    })
+    .then(function (response) {
+      x = response.data[0].lat;
+      y = response.data[0].lon;
 
-        i++;
-        if (i !== 1) return;
+      i++;
+      if (i !== 1) return;
 
-        var params = {
-          lon: y,
-          lat: x,
-        };
-        apiKey = "UBQDIH1h1LCEG7ONpSB5JLLa9lckINJ0FCHnPif0";
-        axios
-          .get(
-            `https://api.nasa.gov/planetary/earth/imagery?lon=${y}&lat=${x}&date=2014-02-01&api_key=${apiKey}`,
-            {
-              params: params,
-            }
-          )
-          .then(function (response) {
-            i = 0;
-            console.log(response);
-            let urlNasa = response.config.url;
-            wrapper.setAttribute("style", "display:flex !important");
-            const img = document.createElement("img");
-            const headline = document.createElement("h2");
-            const divWrapper = document.createElement("div");
-            img.classList.add("nasaImg");
-            img.setAttribute("src", `${urlNasa}`);
-            divWrapper.classList.add("container");
-            wrapper.appendChild(divWrapper);
-            headline.innerText = inputCity;
-            divWrapper.appendChild(headline);
-            divWrapper.appendChild(img);
-            inputCity.value = "";
-            return;
-          })
-          .catch(function (error) {
-            console.log(error);
-          })
-          .then(function () {});
-      })
+      var params = {
+        lon: y,
+        lat: x,
+      };
+      apiKey = "UBQDIH1h1LCEG7ONpSB5JLLa9lckINJ0FCHnPif0";
+      axios
+        .get(
+          `https://api.nasa.gov/planetary/earth/imagery?lon=${y}&lat=${x}&date=2014-02-01&api_key=${apiKey}`,
+          {
+            params: params,
+          }
+        )
+        .then(function (response) {
+          i = 0;
+          console.log(response);
+          let urlNasa = response.config.url;
+          wrapper.setAttribute("style", "display:flex !important");
+          const img = document.createElement("img");
+          const headline = document.createElement("h2");
+          const divWrapper = document.createElement("div");
+          img.classList.add("nasaImg");
+          img.setAttribute("src", `${urlNasa}`);
+          divWrapper.classList.add("container");
+          wrapper.appendChild(divWrapper);
+          headline.innerText = inputCity;
+          divWrapper.appendChild(headline);
+          divWrapper.appendChild(img);
+          inputCity.value = "";
+          return;
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+        .then(function () {});
+    })
 
-      .catch(function (error) {
-        console.log(error);
-        alert("error, wrong latitude or longitude, please try again");
-        return;
-      })
-      .then(function () {});
-  } else return;
+    .catch(function (error) {
+      console.log(error);
+      alert("error, wrong latitude or longitude, please try again");
+      return;
+    })
+    .then(function () {});
 });
